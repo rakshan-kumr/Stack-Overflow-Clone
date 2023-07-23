8bd79bc77827db580bd68499f7b60913a1ea3674
rakshan-kumr <rkshnkumr@gmail.com>
2023-07-23 15:07:03 +0530

    fixed Question details page refresh bug

--- client/src/pages/Questions/QuestionsDetails.jsx
+++ client/src/pages/Questions/QuestionsDetails.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { useParams, Link, useNavigate } from 'react-router-dom'
 import Avatar from '../../components/Avatar/Avatar'
 import DisplayAnswer from './DisplayAnswer'
+import moment from 'moment'
 import upVote from '../../assets/sort-up.svg'
 import downVote from '../../assets/sort-down.svg'
 import './Questions.css'
@@ -16,9 +17,24 @@ const QuestionsDetails = () => {
 
   const navigate = useNavigate()
   const [answer, setAnswer] = useState('')
-  const questionsList = questionsListObject.data
+  const questionsList = questionsListObject
 
-  const question = questionsList.find((question) => question._id === id)
+  // const question = questionsListObject.data.find(
+  //   (question) => question._id === id
+  // )
+
+  const handleUpVote = () => {
+    return
+  }
+  const handleDownVote = () => {
+    return
+  }
+  const handleShare = () => {
+    return
+  }
+  const handleDelete = () => {
+    return
+  }
 
   const handlePostAnswer = (e, answerLength) => {
     e.preventDefault()
@@ -45,95 +61,128 @@ const QuestionsDetails = () => {
   }
   return (
     <div className='question-details-page'>
-      <br />
-      <br />
-      {!question ? (
+      {questionsList.data === null ? (
         <h1>Loading...</h1>
       ) : (
         <>
-          <section className='question-details-container'>
-            <h1>{question.questionTitle}</h1>
-            <div className='question-details-container-2'>
-              <div className='question-votes'>
-                <img src={upVote} alt='' width='18' />
-                <p>{question.upVote - question.downVote}</p>
-                <img src={downVote} alt='' width='18' />
-              </div>
-              <div style={{ width: '100%' }}>
-                <p className='question-body'>{question.questionBody}</p>
-                <div className='question-details-tags'>
-                  {question.questionTags.map((tag) => (
-                    <p key={tag}>{tag}</p>
-                  ))}
-                </div>
-                <div className='question-actions-user'>
-                  <div>
-                    <button type='button'>Share</button>
-                    <button type='button'>Delete</button>
+          {questionsList.data
+            .filter((question) => question._id === id)
+            .map((question) => (
+              <div key={question._id}>
+                <section className='question-details-container'>
+                  <h1>{question.questionTitle}</h1>
+                  <div className='question-details-container-2'>
+                    <div className='question-votes'>
+                      <img
+                        src={upVote}
+                        alt=''
+                        width='18'
+                        className='votes-icon'
+                        onClick={handleUpVote}
+                      />
+                      <p>{question.upVote.length - question.downVote.length}</p>
+                      <img
+                        src={downVote}
+                        alt=''
+                        width='18'
+                        className='votes-icon'
+                        onClick={handleDownVote}
+                      />
+                    </div>
+                    <div style={{ width: '100%' }}>
+                      <p className='question-body'>{question.questionBody}</p>
+                      <div className='question-details-tags'>
+                        {question.questionTags.map((tag) => (
+                          <p key={tag}>{tag}</p>
+                        ))}
+                      </div>
+                      <div className='question-actions-user'>
+                        <div>
+                          <button type='button' onClick={handleShare}>
+                            Share
+                          </button>
+                          {User?.result?._id === question?.userId && (
+                            <button type='button' onClick={handleDelete}>
+                              Delete
+                            </button>
+                          )}
+                        </div>
+                        <div>
+                          <p>asked {moment(question.askedOn).fromNow()}</p>
+                          <Link
+                            to={`/Users/${question.userId}`}
+                            className='user-link'
+                            style={{ color: '#0086d8' }}
+                          >
+                            <Avatar
+                              backgroundColor='orange'
+                              px='8px'
+                              py='5px'
+                              borderRadius='4px'
+                            >
+                              {question.userPosted.charAt(0).toUpperCase()}
+                            </Avatar>
+                            <div>{question.userPosted}</div>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div>
-                    <p>asked {question.askedOn}</p>
+                </section>
+                {question.noOfAnswers !== 0 && (
+                  <section>
+                    <h3>{question.noOfAnswers} Answers</h3>
+                    <DisplayAnswer
+                      key={question._id}
+                      question={question}
+                      handleShare={handleShare}
+                    />
+                  </section>
+                )}
+                <section className='post-ans-container'>
+                  <h3>Your Answer</h3>
+                  <form
+                    onSubmit={(e) => {
+                      handlePostAnswer(e, question.answer.length)
+                    }}
+                  >
+                    <textarea
+                      name=''
+                      id=''
+                      cols='30'
+                      rows='10'
+                      value={answer}
+                      onChange={(e) => setAnswer(e.target.value)}
+                    ></textarea>
+                    <br />
+                    <input
+                      type='submit'
+                      className='post-ans-btn'
+                      value='Post Your Answer'
+                    />
+                  </form>
+                  <p>
+                    Browse other Question tagged
+                    {question.questionTags.map((tag) => (
+                      <Link to='/Tags' key={tag} className='ans-tags'>
+                        {' '}
+                        {tag}{' '}
+                      </Link>
+                    ))}{' '}
+                    or
                     <Link
-                      to={`/User/${question.userId}`}
-                      className='user-link'
-                      style={{ color: '#0086d8' }}
+                      to='/AskQuestion'
+                      style={{ textDecoration: 'none', color: '#009dff' }}
                     >
-                      <Avatar
-                        backgroundColor='orange'
-                        px='8px'
-                        py='5px'
-                        borderRadius='13%'
-                      >
-                        {question.userPosted.charAt(0).toUpperCase()}
-                      </Avatar>
-                      <div>{question.userPosted}</div>
+                      {' '}
+                      ask your own question.
                     </Link>
-                  </div>
-                </div>
+                  </p>
+                </section>
               </div>
-            </div>
-          </section>
-          {question.noOfAnswers !== 0 && (
-            <section>
-              <h3>{question.answer.length} answers</h3>
-              <DisplayAnswer key={question._id} question={question} />
-            </section>
-          )}
+            ))}
         </>
       )}
-      <section className='post-ans-container'>
-        <h3>Your Answer</h3>
-        <form onSubmit={(e) => handlePostAnswer(e, question.answer.length)}>
-          <textarea
-            name=''
-            id=''
-            cols='30'
-            rows='10'
-            onChange={(e) => setAnswer(e.target.value)}
-          ></textarea>
-          <br />
-          <input
-            type='submit'
-            className='post-ans-btn'
-            value='Post your answer'
-          />
-        </form>
-        <p>
-          Browse other questions tagged{' '}
-          {question.questionTags.map((tag) => (
-            <Link to='/Tags' key={tag}>
-              {tag + ' '}
-            </Link>
-          ))}{' '}
-          or{' '}
-          <Link
-            to='/AskQuestion'
-            style={{ textDecoration: 'none', color: '#009dff' }}
-          >
-            ask your own question
-          </Link>
-        </p>
-      </section>
     </div>
   )
 }
